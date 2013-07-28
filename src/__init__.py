a5ca448c69e337e155f8d032fb92e63711609b48
Hakan Dilek <hakandilek@gmail.com>
2013-07-28 21:18:48 +0200

    initial commit

---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+# wifidump module
+
---
+++ src/wifidump.py
@@ -0,0 +1,175 @@
+'''
+Created on Jul 28, 2013
+
+@author: hakan
+'''
+import errno
+import sys
+import traceback
+
+from printer import Printer
+from we import WirelessExtension
+from optparse import OptionParser
+class WifiDumpOptions:
+    """A collection of options to control how the script runs"""
+    def __init__(self):
+        self.iface = 'mon0'
+        self.channel = -1
+        self.channel_hop = True
+        self.max_channel = -1
+        self.timeout = 5
+
+    @staticmethod
+    def create_options():
+        """A class factory which parses command line options and returns an options instance"""
+        parser = OptionParser()
+        parser.add_option('-i', '--iface', dest='iface', default='mon0',
+                          help='Interface to bind to')
+        parser.add_option('-t', '--timeout', dest='timeout', default=5, type='int',
+                          help='Timeout for the channel hop')
+        parser.add_option('-c', '--channel', dest='channel', default=-1, type='int',
+                          help='Channel to bind to')
+        parser.add_option('--max-channel', dest='max_channel', default=-1, type='int',
+                          help='Maximum channel number')
+        options, _ = parser.parse_args()
+
+        dump_options = WifiDumpOptions()
+        dump_options.iface = options.iface
+        dump_options.we = WirelessExtension(dump_options.iface)
+        dump_options.timeout = options.timeout
+        dump_options.channel = options.channel
+        dump_options.channel_hop = (-1 == options.channel)
+        dump_options.max_channel = options.max_channel
+        if -1 == dump_options.max_channel:
+            try:
+                dump_options.max_channel = dump_options.we.get_max_channel()
+                Printer.verbose('CHAN: max_channel[{0}]'.format(dump_options.max_channel), verbose_level=1)
+            except Exception, e:
+                Printer.exception(e)
+                raise
+
+        return dump_options
+
+from scapy.all import sniff
+from scapy.layers.dot11 import Dot11
+from scapy.layers.dot11 import Dot11Auth
+from scapy.layers.dot11 import Dot11Beacon
+from scapy.layers.dot11 import Dot11ProbeReq
+from scapy.layers.dot11 import Dot11ProbeResp
+from scapy.layers.dot11 import RadioTap
+import scapy_ex
+class WifiScanner:
+    def __init__(self, options):
+        self.options = options
+        self.listeners = []
+
+    def attach(self, listener):
+        if not listener in self.listeners:
+            self.listeners.append(listener)
+
+    def detach(self, listener):
+        try:
+            self.listeners.remove(listener)
+        except ValueError:
+            pass
+
+    def notify(self, modifier=None):
+        for listener in self.listeners:
+            if modifier != listener:
+                listener.update(self)
+    
+    def apply_filter(self, packet):
+        try:
+            # Verify the RadioTap header, scanner, and WE all match
+            # if packet.haslayer(RadioTap) and not self.options.input_file:
+                # assert (self.options.channel == packet[RadioTap].Channel), 'got[{0}] expect[{1}]'.format(packet[RadioTap].Channel, self.options.channel)
+
+                # channel = self.options.we.get_channel()
+                # assert (self.options.channel == channel), 'got[{0}] expected[{1}]'.format(channel, self.options.channel)
+
+            # Track AP and STA
+            if packet.haslayer(Dot11):
+#                 
+#                 ap = None
+#                 if packet.haslayer(Dot11Beacon):
+#                     ap = self._update_access_points(packet)
+# 
+#                 elif any(packet.haslayer(layer) for layer in [Dot11ProbeReq, Dot11ProbeResp, Dot11Auth]):
+#                     ap = self._update_access_points(packet)
+#                     self._update_stations(packet)
+# 
+#                 elif packet[Dot11].hasflag('type', 'Data'):
+#                     ap_bssid = packet[Dot11].ap_bssid()
+#                     if ap_bssid in self.access_points:
+#                         self.access_points[ap_bssid].data_count += 1
+
+                Printer.write("Dot11 %s" % packet[Dot11])
+                return True
+
+            # That's unexpected.  print for debugging
+            else:
+                Printer.error(packet.show())
+        except Exception, exc:
+            Printer.exception(exc)
+        finally:
+            return False
+
+    def do_scan(self):
+        timeout = self.options.timeout
+        sniff(iface=self.options.iface,
+              store=False,
+              timeout=timeout,
+              lfilter=self.apply_filter)
+        if timeout:
+            self.options.channel = ((self.options.channel + 3) % self.options.max_channel) + 1
+            self.set_channel(self.options.channel)
+            return True
+        else:
+            return False
+
+    def scan(self):
+        while True:
+            try:
+                
+                if not self.do_scan():
+                    break
+
+            # Exit the scan on a keyboard break
+            except KeyboardInterrupt:
+                break
+
+            # Curses generates system interupt exception (EINTR) when the window is resized
+            except Exception, e:
+                if e.args and e.args[0] == errno.EINTR:
+                    pass
+                else:
+                    Printer.exception(e)
+                    raise
+
+class ConsolePrinter:
+    def update(self):
+        Printer.write('something'.format())
+        
+def main():
+
+    try:
+        options = WifiDumpOptions.create_options()
+        scanner = WifiScanner(options)
+        scanner.attach(ConsolePrinter())
+
+        try:
+            scanner.scan()
+        except Exception, e:
+            Printer.exception(e)
+
+        # Run with or without curses support, but finish in either case by printing a complete report
+        if scanner:
+            scanner.print_results()
+
+    except Exception, e:
+        sys.stderr.write(repr(e))
+        traceback.print_exc(file=sys.stderr)
+
+
+if __name__ == '__main__':
+    main()
